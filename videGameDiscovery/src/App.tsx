57afc738585dc3b40ce2209b6dd4e836be01c686
cypichou <cyprien.godet@gmail.com>
2024-10-04 19:31:08 +0200

    responsive layout

--- videGameDiscovery/src/App.tsx
+++ videGameDiscovery/src/App.tsx
@@ -1,9 +1,28 @@
 import * as React from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
-  return <Button>hop la</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show>
+        <GridItem area="aside" bg="blue">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
