0df8896bb5f15dc6858e5721dd6109776e11b301
cyprien <cyprien.godet@gmail.com>
2024-10-10 11:27:39 +0200

    skeleton

--- videGameDiscovery/src/hooks/useGames.ts
+++ videGameDiscovery/src/hooks/useGames.ts
@@ -23,24 +23,26 @@ export interface Game {
   
 const useGames = () => {
 
-
+    const [isLoading, setLoading] = useState(false)
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
   
     useEffect(() => {
         const controller = new AbortController();
 
+        setLoading(true)
       apiClients
         .get<fetchGamesResponse>("/games", {signal:controller.signal})
-        .then((res) => {setGames(res.data.results)})
+        .then((res) => {setGames(res.data.results); setLoading(false)})
         .catch((err) => {
             if (err instanceof CanceledError) return;
-            setError(err)});
+            setError(err);
+          setLoading(false)});
 
         return ()=> controller.abort()
     }, []);
 
-    return {games, error}
+    return {games, error, isLoading}
 }
 
 export default useGames;    
