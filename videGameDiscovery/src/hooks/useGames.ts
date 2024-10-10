094eef3d650a9ba4412994ee598c2dc3847e4bfc
cyprien <cyprien.godet@gmail.com>
2024-10-10 10:18:45 +0200

    afficher les scores

--- videGameDiscovery/src/hooks/useGames.ts
+++ videGameDiscovery/src/hooks/useGames.ts
@@ -12,7 +12,8 @@ export interface Game {
     id: number;
     name: string;
     background_image:string // bien avoir le meme nom que le parametre dans la requete hehe
-    parent_platforms: {platform: Platform}[]
+    parent_platforms: {platform: Platform}[];
+    metacritic:number
   }
 
   interface fetchGamesResponse {
